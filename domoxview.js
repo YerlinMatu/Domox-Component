e847f5c4fb68c0a8a7260880fe1b7e6eb3527d2e
Yerlin Matu <maicolmatu27@gmail.com>
2017-10-12 16:01:00 -0500

    Add files via upload

--- domoxview.js
+++ domoxview.js
@@ -1,19 +1,21 @@
-/*
-Foco Component.
-@param {String }
+/**
+* Foco Component <Lightbulb pin={n}>.
+* @props { Number } pin
+* @const { Number } pin
+* @description  use for unique light.
 */
 
 class Lightbulb extends React.Component {
-      render(){
-          this.props.pin;
-          return (
-          <div>
-          <label for="flip-select-second"><strong>{ this.props.name}</strong></label>
-          <select name="flip-select" data-role="flipswitch" class="foco">
-            <option>Off</option>
-            <option selected="">On</option>
-          </select>
-          </div>
+    render(){
+      this.props.pin;
+      return (
+      <div>
+        <label for="flip-select-second"><strong>{ this.props.name}</strong></label>
+          <select name="flip-select" data-role="flipswitch" className="foco">
+          <option>Off</option>
+          <option selected="">On</option>
+        </select>
+      </div>
           )
       }
 }
