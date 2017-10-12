6beedd123b51a834e91e8e0fbba79dc7f199032a
Yerlin Matu <maicolmatu27@gmail.com>
2017-10-12 15:28:28 -0500

    Add files via upload

---
+++ domoxview.js
@@ -0,0 +1,14 @@
+class Foco extends React.Component {
+      render(){
+          this.props.pin;
+          return (
+          <div>
+          <label for="flip-select-second"><strong>{ this.props.name}</strong></label>
+          <select name="flip-select" data-role="flipswitch" class="foco">
+            <option>Off</option>
+            <option selected="">On</option>
+          </select>
+          </div>
+          )
+      }
+}
